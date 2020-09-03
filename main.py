cdcb665dfdbcebac35bbdc8af992672473c40f4a
JiarouDeng <dpj5243@psu.edu>
2020-09-03 20:03:40 +0000

    add space

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 #Collaborator: Brooke Bires bmb6210@psu.edu
 #Collaborator: Jimmy Mattison jwm6532@psu.edu
 
-tem = input("Enter temperature:")
-u = input("Enter unit in F/f or C/c:")
+tem = input("Enter temperature: ")
+u = input("Enter unit in F/f or C/c: ")
 if u == "C" or u == "c":
   f= float(float(tem)/5*9)+32;
-  print (f"{tem}° in Celsius is equivalent to {f}°in Fahrenheit.");
+  print (f"{tem}° in Celsius is equivalent to {f}° Fahrenheit.");
 elif u == "F" or u =="f":
-  c= float(float(tem)-32)/5*9;
+  c= float(float(tem)-32)/9*5;
   print(f"{tem}° in Fahrenheit is equivalent to {c}° Celsius.");
 else:
- print(f"Invalid unit({u}).")
+ print(f" Invalid unit({u}).")
  
