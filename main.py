3400e8e96d7f65dc1c66a99a881928043d2b8d95
JiarouDeng <dpj5243@psu.edu>
2020-09-04 17:48:01 +0000

    add space

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#Author: Jiarou Deng dpj5243@psu.edu
-#Collaborator: Qingyao Zhu qvz5130@psu.edu
-#Collaborator: Brooke Bires bmb6210@psu.edu
-#Collaborator: Jimmy Mattison jwm6532@psu.edu
+# Author: Jiarou Deng dpj5243@psu.edu
+# Collaborator: Qingyao Zhu qvz5130@psu.edu
+# Collaborator: Brooke Bires bmb6210@psu.edu
+# Collaborator: Jimmy Mattison jwm6532@psu.edu
 
 tem = float(input("Enter temperature: "))
 u = input("Enter unit in F/f or C/c: ")
